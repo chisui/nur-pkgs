0a0cd449e7d86ca640063d905f2f4a7387ebd7f7
Philipp Dargel <chisui.pd@gmail.com>
2021-11-21 11:48:22 +0100

    zsh: added agnoster theme

--- pkgs/zsh-plugins/default.nix
+++ pkgs/zsh-plugins/default.nix
@@ -5,9 +5,16 @@ with builtins;
 let
   plugins = fromJSON (fileContents ./plugins.json);
   
+  fetchFromGist = { owner, gist, rev, sha256, name, ...}: fetchgit {
+    inherit rev sha256 name;
+    url = "https://gist.github.com/${owner}/${gist}";
+  };
+
   mkSrc = { type, ... }@src:
     if type == "github"
       then fetchFromGitHub (filterAttrs (n: v: n != "type") src)
+    else if type == "gist"
+      then fetchFromGist src
     else
       abort "unknown src type ${type}";
   
